7186fd7e1efdfddc05496a603e06eae790ac8b0d
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-12-10 19:55:02 +0530

    Working with Read Function

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:contact_manager/pages/add_contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
 
+  final contactsCollection = FirebaseFirestore.instance.collection("contacts").
+  snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("My Contacts"),
       ),
+      body:StreamBuilder(
+        stream: contactsCollection,
+        builder: (context, snapshot){
+        if (snapshot.hasData) {
+          return const Text("We have data");
+        } 
+        else if (snapshot.hasError){
+          return const Center(
+            child: Text("There as an error"),
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator.adaptive(),
+        );
+      },
+    ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.push(
