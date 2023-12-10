5ad6e8e8ca7240a24926d871c25dc0882f5bdd0a
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-12-10 20:24:44 +0530

    working with edit function

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,32 +8,62 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
-class _HomePageState extends State<HomePage> {
 
-  final contactsCollection = FirebaseFirestore.instance.collection("contacts").
-  snapshots();
+class _HomePageState extends State<HomePage> {
+  final contactsCollection =
+      FirebaseFirestore.instance.collection("contacts").snapshots();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("My Contacts"),
       ),
-      body:StreamBuilder(
+      body: StreamBuilder(
         stream: contactsCollection,
-        builder: (context, snapshot){
-        if (snapshot.hasData) {
-          return const Text("We have data");
-        } 
-        else if (snapshot.hasError){
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
+            if (documents.isEmpty) {
+              return Center(
+                child: Text(
+                  "No contacts yet",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              );
+            }
+            return ListView.builder(
+              itemCount: documents.length,
+              itemBuilder: (context, index) {
+                final contact = documents[index].data() as Map<String, dynamic>;
+                final contactId = documents[index].id;
+                final String name = contact["name"];
+                final String email = contact['email'];
+                final phone = contact['phone'];
+                final String avatar =
+                    "https://avatars.dicebear.com/api/avataaars/$name.png";
+                return ListTile(
+                  title: Text(name),
+                  subtitle: Text("$phone \n$email"),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                          onPressed: () {}, icon: const Icon(IconlyBroken.edit))
+                    ],
+                  ),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return const Center(
+              child: Text("There as an error"),
+            );
+          }
           return const Center(
-            child: Text("There as an error"),
+            child: CircularProgressIndicator.adaptive(),
           );
-        }
-        return const Center(
-          child: CircularProgressIndicator.adaptive(),
-        );
-      },
-    ),
+        },
+      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.push(
